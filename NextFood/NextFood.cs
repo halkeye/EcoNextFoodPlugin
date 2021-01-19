524891a4510746eb025d263dd3113dc4974a3e96
Gavin Mogan <git@gavinmogan.com>
2021-01-18 18:19:18 -0800

    refactor and add in 1-4 for letting users test the various methods

--- NextFood/NextFood.cs
+++ NextFood/NextFood.cs
@@ -59,89 +59,135 @@ namespace NextFood
 
         public static IEnumerable<StoreComponent> Stores => WorldObjectUtil.AllObjsWithComponent<StoreComponent>();
 
+        public static LocString NextFoodBody(User user, String count = "3")
+        {
+            Dictionary<FoodItem, HashSet<WorldObject>> whereArtFood = new Dictionary<FoodItem, HashSet<WorldObject>>();
+
+            IEnumerable<StoreComponent> storesWithFood = Stores
+                .Where(store => store.Enabled != false)
+                .Where(store => store.StoreData.SellOffers
+                    .Where(item => item.Stack.Quantity > 0)
+                    .Where(item => item.Stack.Item is FoodItem)
+                    .Count() > 0);
+
+            foreach (StoreComponent store in storesWithFood) {
+                IEnumerable<TradeOffer> foodTrades = store.StoreData.SellOffers.Where(item => item.Stack.Quantity > 0).Where(item => item.Stack.Item is FoodItem);
+                foreach (TradeOffer trade in foodTrades)
+                {
+                    FoodItem foodItem = trade.Stack.Item as FoodItem;
+                    if (!whereArtFood.ContainsKey(foodItem))
+                    {
+                        whereArtFood.Add(foodItem, new HashSet<WorldObject>());
+                    }
+                    whereArtFood[foodItem].Add(store.Parent);
+                }
+            }
+
+            IEnumerable<StorageComponent> enumerable = WorldObjectUtil.AllObjsWithComponent<StorageComponent>();
+            IEnumerable<StorageComponent> accessibleStorage = enumerable.Where(i => i.Parent.Auth.Owners != null && i.Parent.Auth.Owners.Contains(user));
+
+            foreach (StorageComponent storage in accessibleStorage)
+            {
+                foreach (ItemStack stack in storage.Inventory.Stacks.Where(item => item.Item is FoodItem))
+                {
+                    FoodItem foodItem = stack.Item as FoodItem;
+                    if (!whereArtFood.ContainsKey(foodItem))
+                    {
+                        whereArtFood.Add(foodItem, new HashSet<WorldObject>());
+                    }
+                    whereArtFood[foodItem].Add(storage.Parent);
+                }
+            }
+
+            IEnumerable<KeyValuePair<FoodItem, float>> possibleBuys = whereArtFood.Keys
+                .ToDictionary(it => it, it => FoodCalculatorComponent.getSkillPointsVariation(user.Player, it as FoodItem))
+                .OrderByDescending(pair => pair.Value)
+                .Take(Int32.Parse(count));
+
+            List<LocString> body = new List<LocString>(); 
+            if (possibleBuys.Count() > 0)
+            {
+                foreach (var possibleBuy in possibleBuys)
+                {
+                    WorldObject store = whereArtFood[possibleBuy.Key].First();
+
+                    LocString itemValue = LocStringExtensions.Style(
+                        Localizer.DoStr(Math.Round(possibleBuy.Value, 2).ToString()),
+                        possibleBuy.Value >= 0 ? Text.Styles.Positive : Text.Styles.Negative
+                    );
+                    String locations = String.Join(", ", whereArtFood[possibleBuy.Key].Select(food => food.UILink()));
+                    body.Add(Localizer.DoStr($"{possibleBuy.Key.UILink()} will give you {itemValue} points and can be found at: {locations}"));
+                }
+            }
+            else
+            {
+                body.Add(Localizer.DoStr("Could not find you any food"));
+            }
+            return Localizer.DoStr(String.Join("\n", body));
+        }
 
         [ChatCommand("Suggests what food you should get next.", "nextfood")]
         public static void NextFoodCmd(User user, String count = "3")
         {
             CallWithErrorHandling<object>((lUser, args) =>
             {
-                Dictionary<FoodItem, HashSet<WorldObject>> whereArtFood = new Dictionary<FoodItem, HashSet<WorldObject>>();
-
-                IEnumerable<StoreComponent> storesWithFood = Stores
-                    .Where(store => store.Enabled != false)
-                    .Where(store => store.StoreData.SellOffers
-                        .Where(item => item.Stack.Quantity > 0)
-                        .Where(item => item.Stack.Item is FoodItem)
-                        .Count() > 0);
-
-                foreach (StoreComponent store in storesWithFood) {
-                    IEnumerable<TradeOffer> foodTrades = store.StoreData.SellOffers.Where(item => item.Stack.Quantity > 0).Where(item => item.Stack.Item is FoodItem);
-                    foreach (TradeOffer trade in foodTrades)
-                    {
-                        FoodItem foodItem = trade.Stack.Item as FoodItem;
-                        if (!whereArtFood.ContainsKey(foodItem))
-                        {
-                            whereArtFood.Add(foodItem, new HashSet<WorldObject>());
-                        }
-                        whereArtFood[foodItem].Add(store.Parent);
-                    }
-                }
-
-                IEnumerable<StorageComponent> enumerable = WorldObjectUtil.AllObjsWithComponent<StorageComponent>();
-                IEnumerable<StorageComponent> accessibleStorage = enumerable.Where(i => i.Parent.Auth.Owners != null && i.Parent.Auth.Owners.Contains(user));
-
-                foreach (StorageComponent storage in accessibleStorage)
-                {
-                    foreach (ItemStack stack in storage.Inventory.Stacks.Where(item => item.Item is FoodItem))
-                    {
-                        FoodItem foodItem = stack.Item as FoodItem;
-                        if (!whereArtFood.ContainsKey(foodItem))
-                        {
-                            whereArtFood.Add(foodItem, new HashSet<WorldObject>());
-                        }
-                        whereArtFood[foodItem].Add(storage.Parent);
-                    }
-                }
-
-                IEnumerable<KeyValuePair<FoodItem, float>> possibleBuys = whereArtFood.Keys
-                    .ToDictionary(it => it, it => FoodCalculatorComponent.getSkillPointsVariation(user.Player, it as FoodItem))
-                    .OrderByDescending(pair => pair.Value)
-                    .Take(Int32.Parse(count));
-
-                ChatManager.ServerMessageToPlayer(Localizer.DoStr(Text.Size(1.5f, Text.ColorUnity(Color.Red.UInt, "=== NextFood(NG) plugin ==="))), user);
-                if (possibleBuys.Count() > 0)
-                {
-                    foreach (var possibleBuy in possibleBuys)
-                    {
-                        WorldObject store = whereArtFood[possibleBuy.Key].First();
-
-                        LocString itemValue = LocStringExtensions.Style(
-                            Localizer.DoStr(Math.Round(possibleBuy.Value, 2).ToString()),
-                            possibleBuy.Value >= 0 ? Text.Styles.Positive : Text.Styles.Negative
-                        );
-                        LocStringBuilder locations = new LocStringBuilder();
-                        foreach (var food in whereArtFood[possibleBuy.Key])
-                        {
-                            if (!locations.Empty)
-                            {
-                                locations.Append(", ");
-                            }
-                            locations.Append(food.UILink());
-                        }
-
-                        ChatManager.ServerMessageToPlayer(Localizer.Format(
-                            "{0} will give you {1} points and can be found at: {2}",
-                            possibleBuy.Key.UILink(),
-                            itemValue,
-                            locations
-                        ), user);
-                    }
-                } else
-                {
-                    ChatManager.ServerMessageToPlayer(Localizer.DoStr("Could not find you any food"), user);
-                }
-            }, user);
+                LocString nextFoodBody = NextFoodBody(lUser, args[0]);
+                ChatManager.ServerMessageToPlayer(Localizer.DoStr(Text.Size(1.5f, Text.ColorUnity(Color.Red.UInt, "=== NextFood(NG) plugin ==="))), lUser);
+                ChatManager.ServerMessageToPlayer(nextFoodBody, lUser);
+            }, user, count);
         }
 
+
+        [ChatCommand("Suggests what food you should get next.", "nextfood1")]
+        public static void NextFoodCmd1(User user, String count = "3")
+        {
+            CallWithErrorHandling<object>((lUser, args) =>
+            {
+                LocString nextFoodBody = NextFoodBody(lUser, args[0]);
+                ChatManager.ServerMessageToPlayer(Localizer.DoStr(Text.Size(1.5f, Text.ColorUnity(Color.Red.UInt, "=== NextFood(NG) plugin ==="))), lUser);
+                ChatManager.ServerMessageToPlayer(nextFoodBody, lUser);
+            }, user, count);
+        }
+
+        [ChatCommand("Suggests what food you should get next.", "nextfood2")]
+        public static void NextFoodCmd2(User user, String count = "3")
+        {
+            CallWithErrorHandling<object>((lUser, args) =>
+            {
+                LocString nextFoodBody = NextFoodBody(lUser, args[0]);
+                user.Player.InfoBoxLocStr(
+                    Localizer.DoStr(
+                        Text.Size(1.5f, Text.ColorUnity(Color.Red.UInt, "=== NextFood(NG) plugin ===")) + "\n" +
+                        nextFoodBody
+                     )
+                 );
+            }, user, count);
+        }
+
+
+        [ChatCommand("Suggests what food you should get next.", "nextfood3")]
+        public static void NextFoodCmd3(User user, String count = "3")
+        {
+            CallWithErrorHandling<object>((lUser, args) =>
+            {
+                LocString nextFoodBody = NextFoodBody(lUser, args[0]);
+                lUser.Player.OkBox(
+                    Localizer.DoStr(
+                        Text.Size(1.5f, Text.ColorUnity(Color.Red.UInt, "=== NextFood(NG) plugin ===")) + "\n" +
+                        nextFoodBody
+                     )
+                 );
+            }, user, count);
+        }
+
+        [ChatCommand("Suggests what food you should get next.", "nextfood4")]
+        public static void NextFoodCmd4(User user, String count = "3")
+        {
+            CallWithErrorHandling<object>((lUser, args) =>
+            {
+                LocString nextFoodBody = NextFoodBody(lUser, args[0]);
+                lUser.Player.OpenInfoPanel(Localizer.DoStr("What should you eat next?"), nextFoodBody, "");
+            }, user, count);
+        }
     }
 }
